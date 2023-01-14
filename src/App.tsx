7aeb316164d0f1d918f0c5f24f5be0d01b12e14b
neicore <neicoreadams@gmail.com>
2023-01-14 22:39:45 +0300

    reducer example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,105 @@
+import { useReducer, useState } from 'react'
+
+interface StateType {
+  count: number
+}
+
+interface Actions {
+  type: 'initialize' | 'increment' | 'decrement'
+  payload: StateType
+}
+
+const reducer = (state: StateType, action: Actions) => {
+  const { count } = state
+  const { type, payload } = action
+
+  switch (type) {
+    case 'initialize':
+      return {
+        ...state,
+        count: payload?.count,
+      }
+
+    case 'increment':
+      return {
+        ...state,
+        count: count + 1,
+      }
+
+    case 'decrement':
+      return {
+        ...state,
+        count: count - 1,
+      }
+
+    default:
+      return state
+  }
+}
+
+const initialState = {
+  count: 0,
+} satisfies StateType
+
 function App() {
-  return <div className="App">Life has no meaning</div>
+  const [input, setInput] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div className="App">
+      <h1>useReducer Example</h1>
+
+      <div>
+        <label>Start Count</label>
+        <input
+          type="number"
+          onChange={(e) => setInput(parseInt(e.target.value))}
+          value={input}
+        />
+        <br />
+        <button
+          onClick={() =>
+            dispatch({
+              type: 'initialize',
+              payload: {
+                count: input,
+              },
+            })
+          }
+        >
+          Initialize Counter
+        </button>
+      </div>
+
+      <p>{state.count}</p>
+      <div>
+        <button
+          onClick={() =>
+            dispatch({
+              type: 'increment',
+              payload: {
+                count: state.count,
+              },
+            })
+          }
+        >
+          Increment
+        </button>
+        <button
+          onClick={() =>
+            dispatch({
+              type: 'decrement',
+              payload: {
+                count: state.count,
+              },
+            })
+          }
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
